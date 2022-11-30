7d697a3b7f45d7fcd89c019721d1eb8587515e87
irfan <ipekusic@yahoo.com>
2022-11-30 13:42:29 -0500

    single car detials written out

--- src/components/SingleCar.jsx
+++ src/components/SingleCar.jsx
@@ -7,6 +7,7 @@ const SingleCar = (props) => {
   return (
     <div id="singleCar">
         <h2>This is the SingleCar component, used to render on the all cars page from the map, each single car should contain some sort of button or navlink to single car details</h2>
+        <div>{car.photo_url}</div>
         <div>{car.make}</div>
         <div>{car.model}</div>
         <div>{car.year}</div>
--- src/components/SingleCarDetails.jsx
+++ src/components/SingleCarDetails.jsx
@@ -1,10 +1,82 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-const SingleCarDetails = () => {
+const SingleCarDetails = (props) => {
+  const {id} = useParams();
+  const car = props.filterCars(id)[0]
+  const navigate = useNavigate();
+  const [carDetails, setCarDetails] = useState({
+        type: '',
+        make: '',
+        model: '',
+        year: '',
+        color: '',
+        price: '',
+        transmission_type: '',
+        mileage: '',
+        interior_color: '',
+        doors: '',
+        seats: '',
+        mpg: '',
+        inventory: '',
+        photo_url: '',
+        drive_type: '',
+        new_used: '',
+  })
+  useEffect(()=>{
+    car?
+    setCarDetails(
+      {        
+      type: car.type,
+      make: car.make,
+      model: car.model,
+      year: car.year,
+      color: car.color,
+      price: car.price,
+      transmission_type: car.transmission_type,
+      mileage: car.mileage,
+      interior_color: car.interior_color,
+      doors: car.doors,
+      seats: car.seats,
+      mpg: car.mpg,
+      inventory: car.inventory,
+      photo_url: car.photo_url,
+      drive_type: car.drive_type,
+      new_used: car.new_used,}
+    ): null
+  }, [car])
   return (
     <div id="singleCarDetails">
         <h2>This is the SingleCarDetails component, the page where we show more info about a single car</h2>
+  {car ? (<>
+    <div className="box">
+     <div>{car.seller} </div>
+     <div>{car.type} </div>
+     <div>{car.make} </div>
+     <div>{car.model} </div>
+     <div>{car.year} </div>
+     <div>{car.color} </div>
+     <div>{car.price} </div>
+     <div>{car.transmission_type} </div>
+     <div>{car.mileage} </div>
+     <div>{car.interior_color} </div>
+     <div>{car.doors} </div>
+     <div>{car.seats} </div>
+     <div>{car.mpg} </div>
+     <div>{car.inventory} </div>
+     <div>{car.photo_url} </div>
+     <div>{car.drive_type} </div>
+     <div>{car.new_used} </div>
+    </div>
+    
+    </>
+    ):(
+      <div> loading the cars... </div>
+      )}
   </div>
+
+    
+  
   );
 };
 
