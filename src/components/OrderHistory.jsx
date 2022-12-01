1050fa4f1b972cfb571e0d3cf9c4f6b07dc8d7b5
Alex <alexwinston2123@icloud.com>
2022-12-01 14:18:20 -0600

    added order history to my account

--- src/components/OrderHistory.jsx
+++ src/components/OrderHistory.jsx
@@ -16,6 +16,16 @@ fetchData();
 
   return (
     <div id="orderHistory">
+      {pastOrders ? pastOrders.map((element)=>{
+        return(
+          <div key={element.id}>Order Number: {element.id} 
+          {element.cars.map((car)=>{
+            return(
+              <div key={car.id}>{car.make} {car.model} {car.year} {car.price}</div>
+            )
+          })}</div>)
+      }): null}
+
         <h2>This is the OrderHistory component</h2>
   </div>
   );
