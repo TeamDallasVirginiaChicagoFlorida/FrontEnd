7e34c9628d5e94b8d5261913b8b566f70d14b9e3
Leonardo <cruzleonardo831@gmail.com>
2022-12-01 10:15:55 -0600

    delete car button kind of working

--- src/components/Cart_SingleCar.jsx
+++ src/components/Cart_SingleCar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { deleteCar, deleteCarFromCart } from "../api-adapter";
 
 const Cart_SingleCar = (props) => {
     const item = props.item
+    async function removeCar(){
+        await deleteCarFromCart(item.id)};
+
 return(
     <div id="adminAddCar">
     <img src={item.photo_url} />
     <p>{item.make} {item.model} {item.year}</p>
     <p>{item.price}</p>
+    <button onClick={removeCar}>Remove car from cart</button>
 </div>
 )
 }
