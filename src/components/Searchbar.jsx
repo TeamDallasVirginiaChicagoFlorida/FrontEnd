79c78ada15385f8f346d874922d6a5c4fd449dc1
Leonardo <cruzleonardo831@gmail.com>
2022-12-01 08:38:08 -0600

    started working on search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,11 +1,57 @@
-import React from "react";
+// import React, { useState, useEffect } from "react";
+// import { getAllCars } from "../api-adapter";
 
-const Searchbar = () => {
-  return (
-    <div id="searchbar">
-        <h2>This is the searchbar component</h2>
-  </div>
-  );
-};
+// const SearchBar = () => {
+//   const [vehicles, setVehicles] = useState([]);
+//   const [searchTerm, setSearchTerm] = useState("");
 
-export default Searchbar;
+//   useEffect(() => {
+//     async function fetchData() {
+//       const allData = await getAllCars(setVehicles);
+//     }
+//     fetchData();
+//   }, []);
+
+//   function searchMatches(vehicle, text) {
+//     return (
+//       vehicle.make.toLowerCase().includes(text)
+//     );
+//   }
+//   const filteredSearch = vehicles.filter((vehicle) =>
+//   searchMatches(vehicle, searchTerm));
+//   const vehiclesToDisplay = searchTerm.length ? filteredSearch : vehicles;
+  
+//   return (
+//     <div id="searchbar">
+//       <p className="titleText">Search Vehicles: </p>
+//       <span className="Search">
+//         <input
+//         type="text"
+//         className="searchbar"
+//         placeholder="Search..."
+//         value={searchTerm}
+//         onChange={(event)=>{
+//           setSearchTerm(event.target.value);
+//         }}
+//         />
+//       </span>
+//       <div className="box">
+//         {vehiclesToDisplay.map((vehicle)=>{
+//           return(
+//             <div className="vehicleBox">
+//               <div className="carMake">Make: {vehicle.make}</div>
+//               <div className="carModel">Model: {vehicle.model}</div>
+//               <div className="carYear">Year: {vehicle.year}</div>
+
+
+//             </div>  
+//           )
+//         })}
+
+//       </div>
+//     </div>
+   
+//   );
+// };
+
+// export default SearchBar;
