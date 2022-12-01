c949cf338730c61f815093c03c3baa49ff5de0a2
Lex Mullin <alemulli@gmail.com>
2022-12-01 11:27:05 -0500

    removing cars from cart rerenders now, but the total price does not update, so 90% functionality

--- src/components/Cart_SingleCar.jsx
+++ src/components/Cart_SingleCar.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { deleteCar, deleteCarFromCart } from "../api-adapter";
 
 const Cart_SingleCar = (props) => {
-    const item = props.item
-    async function removeCar(){
-        await deleteCarFromCart(item.id)};
+  const [item, setItem] = useState(props.item);
+  async function removeCar() {
+    const response = await deleteCarFromCart(item.id);
 
-return(
+    !response.error && setItem(false)
+  }
+
+  return (
     <div id="adminAddCar">
-    <img src={item.photo_url} />
-    <p>{item.make} {item.model} {item.year}</p>
-    <p>{item.price}</p>
-    <button onClick={removeCar}>Remove car from cart</button>
-</div>
-)
-}
+        {item ?         <>
+      <img src={item.photo_url} />
+      <p>
+        {item.make} {item.model} {item.year}
+      </p>
+      <p>{item.price}</p>
+      <button onClick={removeCar}>Remove car from cart</button>
+      </>:null}
 
-export default Cart_SingleCar
+    </div>
+  );
+};
+
+export default Cart_SingleCar;
