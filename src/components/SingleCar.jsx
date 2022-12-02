536e2740a1722e3e0c589c0212e507cfe260e271
Leonardo <cruzleonardo831@gmail.com>
2022-12-02 13:45:15 -0600

    some progess on cart.jsx

--- src/components/SingleCar.jsx
+++ src/components/SingleCar.jsx
@@ -23,8 +23,6 @@ const SingleCar = (props) => {
         }else{
           localStorage.setItem("cart", JSON.stringify([car.id]))
         }
-        // const carts = localStorage.getItem('carts');
-        // return carts.some(cart => cart.id == id);
       }
     }
   
@@ -46,39 +44,5 @@ const SingleCar = (props) => {
 };
 
 
-// async function addCar() {
-//   if (isLoggedIn) {
-//     const response = await addCarToCart(car.id, currentId);
-
-//     !response.error && setCar(false);
-//   } else {
-//     // 1. Check to see if there is a cart in Local Storage
-//     // 2. If not, create a new cart in Local Storage
-//     // 3. Once you have your cart array, you can push your new cart_item to the array
-//     // example
-//     const newItem = {make: car.make, model: car.model, price: car.price}
-//     const currentCart = localStorage.getItem('cart')
-//     currentCart.push(newItem)
-
-// }
-
-// return (
-//   <div id="singleCar">
-//     {car ? (
-//       <>
-//         <h2>
-//           {car.make} {car.year} {car.model}
-//         </h2>
-//         <img src={car.photo_url} />
-//         <h3>${car.price}</h3>
-//         <Link to={`/carDetails/${car.id}`}>
-//           <button> See More Details</button>
-//         </Link>
-//         <button onClick={addCar}>Add To Cart</button>
-//       </>
-//     ) : null}
-//   </div>
-// );
-// };
 
 export default SingleCar;
