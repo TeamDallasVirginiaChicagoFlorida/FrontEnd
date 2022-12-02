2b842ea8d1e5e034a87dd5e49a6cd6c1fc0c7d6b
Leonardo <cruzleonardo831@gmail.com>
2022-12-02 13:47:33 -0600

    some changes to cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,42 +4,54 @@ import { Cart_SingleCar } from "./";
 import { useNavigate } from "react-router-dom";
 
 const Cart = (props) => {
-  const [activeCartCars, setActiveCartCars] = useState([])
-  const [currentId, setCurrentId] = useState()
-  const isLoggedIn = props.isLoggedIn
+  const [activeCartCars, setActiveCartCars] = useState([]);
+  const [currentId, setCurrentId] = useState();
+  const isLoggedIn = props.isLoggedIn;
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getCartByUser();
-        const activeCart = data[0].cars;
-        const id= data[0].id
-        setCurrentId(id)
-  setActiveCartCars(activeCart)
+      const activeCart = data[0].cars;
+      const id = data[0].id;
+      setCurrentId(id);
+      setActiveCartCars(activeCart);
     };
     fetchData();
   }, [isLoggedIn]);
 
- let total = 0
+  let total = 0;
 
- async function finishedSale(){
-  await checkOut(currentId)
-  navigate("/myaccount")
- }
+  async function finishedSale() {
+    await checkOut(currentId);
+    navigate("/myaccount");
+  }
   return (
     <div id="cart">
-      <h1>Your Cart:</h1>
-      {activeCartCars 
-        ? activeCartCars.map((item) => {
-            return <Cart_SingleCar key={item.id} item={item} />;
-          })
-        : null}
-        <h3>Total Price: {
-          activeCartCars.map((item)=>{
-            total += item.price
-          })}${parseInt(total, 10)}</h3>
-        <button onClick={finishedSale}>Check Out</button>  
+      {isLoggedIn ? (
+        <div>
+          <h1>Your Cart:</h1>
+          {activeCartCars
+            ? activeCartCars.map((item) => {
+                return <Cart_SingleCar key={item.id} item={item} />;
+              })
+            : null}
+          <h3>
+            Total Price:{" "}
+            {activeCartCars.map((item) => {
+              total += item.price;
+            })}
+            ${parseInt(total, 10)}
+          </h3>
+          <button onClick={finishedSale}>Check Out</button>
+        </div>
+      ) : (
+        <div>
+          
+          <h1>Your Cart:</h1>
+        </div>
+      )}
     </div>
   );
 };
