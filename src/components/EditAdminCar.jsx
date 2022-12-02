e99a81d44cf2c8a28a66310a6fb0d5234f105420
Lex Mullin <alemulli@gmail.com>
2022-12-02 10:45:36 -0500

    added onchange and useState to the EditAdminCar component will come back for onsubmit shortly

--- src/components/EditAdminCar.jsx
+++ src/components/EditAdminCar.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 const EditAdminCar = (props) => {
     const setEditCar = props.setEditCar
+    const user = props.user
+    const car = props.car
+
     async function closeEditCarMenu(){
         setEditCar(false)
     }
+
+    const [editCarInfo, setEditCarInfo] = useState({
+      seller: user.id, 
+      type: car.type, 
+      make: car.make, 
+      model: car.model, 
+      year:  car.year,
+      color: car.color,
+      price: car.price, 
+      transmission_type: car.trasmission_type, 
+      mileage: car.mileage, 
+      interior_color: car.interior_color, 
+      doors: car.doors, 
+      seats: car.seats, 
+      mpg: car.mpg, 
+      inventory: 1, 
+      photo_url: car.photo_url, 
+      drive_type: car.drive_type, 
+      new_used: car.new_used,
+    })
+
     return(
         <div id="editAdminCar" >
             <div className="popupMenu">
@@ -19,55 +43,69 @@ const EditAdminCar = (props) => {
       <form>
         <span>
         <label htmlFor="type">Type: </label>
-        <select>
+        <select id="type" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, type: e.target.value })
+            } required>
           <option value="Coupe">Coupe</option>
           <option value="Sedan">Sedan</option>
           <option value="SUV">SUV</option>
           <option value="Van">Van</option>
           <option value="Truck">Truck</option>
-          <option value="HatchbackHatchback"></option>
+          <option value="Hatchback">Hatchback</option>
         </select>
         </span>
         <br />
 
         <span>
         <label for="make">Make: </label>
-        <input id="make" />
+        <input id="make" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, make: e.target.value })
+            } required/>
         </span>
         <br />
 
 
         <span>
         <label for="model">Model: </label>
-        <input id="model" />
+        <input id="model" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, model: e.target.value })
+            } required/>
         </span>
         <br />
 
 
         <span>
         <label for="year">Year: </label>
-        <input type="number" id="year" />
+        <input type="number" id="year" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, year: e.target.value })
+            } required />
         </span>
         <br />
 
 
         <span>
-        <label for="color">Make: </label>
-        <input id="color" />
+        <label for="color">Color: </label>
+        <input id="color" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, color: e.target.value })
+            } required/>
         </span>
         <br />
 
 
         <span>
         <label for="price">Price: </label>
-        <input type="number" id="price" />
+        <input type="number" id="price" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, price: e.target.value })
+            } required/>
         </span>
         <br />
 
 
         <span>
         <label htmlFor="transmission_type">Transmission: </label>
-        <select id="transmission_type">
+        <select id="transmission_type" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, transmission_type: e.target.value })
+            } required>
           <option value="Automatic">Automatic</option>
           <option value="Manual">Manual</option>
         </select>
@@ -77,35 +115,45 @@ const EditAdminCar = (props) => {
 
         <span>
         <label htmlFor="mileage">Mileage: </label>
-        <input type="number" id="mileage" />
+        <input type="number" id="mileage" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, mileage: e.target.value })
+            } required/>
         </span>
         <br />
 
 
         <span>
         <label htmlFor="interior_color">Interior Color: </label>
-        <input id="interior_color" />
+        <input id="interior_color" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, interior_color: e.target.value })
+            } required/>
         </span>
         <br />
 
 
         <span>
         <label htmlFor="seats">Seats: </label>
-        <input type="number" id="seats" />
+        <input type="number" id="seats" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, seats: e.target.value })
+            } required/>
         </span>
         <br />
 
 
         <span>
         <label htmlFor="doors">Doors: </label>
-        <input type="number" id="doors" />
+        <input type="number" id="doors" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, doors: e.target.value })
+            } required/>
         </span>
         <br />
 
 
         <span>
         <label htmlFor="mpg">MPG: </label>
-        <input type="number" id="mpg" />
+        <input type="number" id="mpg" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, mpg: e.target.value })
+            } required/>
         </span>
         <br />
 
@@ -119,7 +167,9 @@ const EditAdminCar = (props) => {
 
         <span>
         <label htmlFor="drive_type">Drive Type:</label>
-        <select id="drive_type">
+        <select id="drive_type" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, drive_type: e.target.value })
+            } required>
           <option value="4WD">4WD</option>
           <option value="FWD">FWD</option>
           <option value="RWD">RWD</option>
@@ -130,7 +180,9 @@ const EditAdminCar = (props) => {
 
         <span>
         <label htmlFor="new_used">Condition: </label>
-        <select id="new_used">
+        <select id="new_used" onChange={(e) =>
+              setEditCarInfo({ ...editCarInfo, new_used: e.target.value })
+            } required>
           <option value="new">New</option>
           <option value="used">Used</option>
         </select>
