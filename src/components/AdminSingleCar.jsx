a286d17eba20114af12def95fef7bcf8d1e53bee
Lex Mullin <alemulli@gmail.com>
2022-12-05 12:22:42 -0500

    delete car from sellers cars for sale list functionality added

--- src/components/AdminSingleCar.jsx
+++ src/components/AdminSingleCar.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
 import { EditAdminCar } from "./"
+import { deleteCar } from "../api-adapter";
 
 const AdminSingleCar = (props) => {
     const setEditCar = props.setEditCar
     const user = props.user
-    const car = props.car
+    const [car, setCar] = useState(props.car)
     const editCar = props.editCar
     const setCarBeingEdited = props.setCarBeingEdited
 
@@ -15,17 +16,23 @@ const AdminSingleCar = (props) => {
         setEditCar(true)
       }
 
+      async function deleteTheCar () {
+        const response = await deleteCar(car.id)
+        !response.error && setCar(false)
+      }
+
   return (
     <div id="adminSingleCar">
-      <div className="adminCarInfo">
+      {car ?  <div className="adminCarInfo">
         <img className="tinyimage" src={car.photo_url} /> {car.make} {car.model} {car.year} ${car.price} 
       
       <div className="adminButtons"><span className="addToCartPlus" onClick={openEditCarMenu}><span className="material-symbols-outlined">
 edit
 </span> Edit </span>{" "}
-      <span className="addToCartPlus"><span className="material-symbols-outlined">
+      <span onClick={deleteTheCar} className="addToCartPlus"><span className="material-symbols-outlined">
 delete_forever
-</span> Remove</span></div></div>
+</span> Remove</span></div></div>: null}
+     
     </div>
   );
 };
