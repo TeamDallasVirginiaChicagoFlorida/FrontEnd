c6edb4fdf441185e987134ae47dfb2c4c4147ba7
Lex Mullin <alemulli@gmail.com>
2022-12-05 14:28:17 -0500

    fixed the component SingleCar that got overwritten by  an older version

--- src/components/SingleCar.jsx
+++ src/components/SingleCar.jsx
@@ -6,72 +6,45 @@ const SingleCar = (props) => {
   const [car, setCar] = useState(props.car);
   const isLoggedIn = props.isLoggedIn;
   const currentId = props.currentId
+  
 
 
   async function addCar() {
-    if(isLoggedIn){
-
-    
+    if(localStorage.getItem("token")){
     const response = await addCarToCart(car.id, currentId);
-
     !response.error && setCar(false)}
     else{
-        const carts = localStorage.getItem('carts');
-        return carts.some(cart => cart.id == id);
+        if(localStorage.getItem("cart")){
+          const holdThisCart = JSON.parse(localStorage.getItem("cart"))
+          if(!holdThisCart.includes(car.id)){
+            holdThisCart.push(car.id)
+            localStorage.setItem("cart", JSON.stringify(holdThisCart))
+          }
+        }else{
+          localStorage.setItem("cart", JSON.stringify([car.id]))
+        }
       }
     }
   
 
   return (
     <div id="singleCar">
-      {car ?  <><h2>
+      {car ?  <>
+        
+      <h2>
         {car.make} {car.year} {car.model}
       </h2>
-      <img src={car.photo_url} />
+      <img src={car.photo_url} className="AllCarsPage" />
       <h3>${car.price}</h3>
       <Link to={`/carDetails/${car.id}`}>
-        <button> See More Details</button>
+        <button className="SeeMoreDetailsBtn"><span> See More Details</span></button>
       </Link>
-      <button onClick={addCar}>Add To Cart</button></>:null}
+      <span onClick={addCar} className="addToCartPlus"><span  className="material-symbols-outlined addToCart">
+add_shopping_cart
+</span> Add to Cart</span></>:null}
      
     </div>
   );
-};
-
-
-async function addCar() {
-  if (isLoggedIn) {
-    const response = await addCarToCart(car.id, currentId);
-
-    !response.error && setCar(false);
-  } else {
-    // 1. Check to see if there is a cart in Local Storage
-    // 2. If not, create a new cart in Local Storage
-    // 3. Once you have your cart array, you can push your new cart_item to the array
-    // example
-    const newItem = {make: car.make, model: car.model, price: car.price}
-    const currentCart = localStorage.getItem('cart')
-    currentCart.push(newItem)
-
-}
-
-return (
-  <div id="singleCar">
-    {car ? (
-      <>
-        <h2>
-          {car.make} {car.year} {car.model}
-        </h2>
-        <img src={car.photo_url} />
-        <h3>${car.price}</h3>
-        <Link to={`/carDetails/${car.id}`}>
-          <button> See More Details</button>
-        </Link>
-        <button onClick={addCar}>Add To Cart</button>
-      </>
-    ) : null}
-  </div>
-);
 };
 
 export default SingleCar;
